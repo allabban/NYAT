a4146bbc5e51756b0443a70597594592b2fc6951
allabban <abdallahallabban12@gmail.com>
2025-02-20 15:43:19 +0300

    3

--- Hafta01/src/Daire.java
+++ Hafta01/src/Daire.java
@@ -1,9 +1,18 @@
 public class Daire {
-    double yaricap;
-    double AlanHisapla(){
+    private double yaricap;
+    public Daire(double yaricap){
+        yaricap=yaricap;
+    }
+    private double AlanHisapla(){
         return Math.PI * yaricap * yaricap;
     }
-    Double CevreHisapla(){
+    private Double CevreHisapla(){
         return 2 * Math.PI * yaricap;
+    }
+    public void YazdirAlan(){
+        System.out.println("Alan: " + AlanHisapla());
+    }
+    public void YazdirCevre(){
+        System.out.println("Cevre: " + CevreHisapla());
     }
 }
--- Hafta01/src/Hello.java
+++ Hafta01/src/Hello.java
@@ -1,16 +1,21 @@
 public class Hello {
     public static void main(String[] args) {
-        Daire daire=new Daire();
-        daire.yaricap=6;
-        System.out.println("Dairenin alani :"+ Math.PI*daire.yaricap*daire.yaricap);
-        System.out.println("Dairenin cevresi:"+2*Math.PI*daire.yaricap);
-        Daire daire2=new Daire();
-        daire2.yaricap=5;
-        System.out.println("Dairenin alani :"+ Math.PI*daire2.yaricap*daire2.yaricap);
-        System.out.println("Dairenin cevresi:"+2*Math.PI*daire2.yaricap);
-        Daire daire3=new Daire();
-        daire3.yaricap=4;
-        System.out.println("Dairenin alani :"+ Math.PI*daire3.yaricap*daire3.yaricap);
-        System.out.println("Dairenin cevresi:"+2*Math.PI*daire3.yaricap);
+        Daire daire=new Daire(6);
+        //
+        daire.YazdirAlan();
+        daire.YazdirCevre();
+        Daire daire2=new Daire(5);
+        //daire2.yaricap=5;
+        daire2.YazdirAlan();
+        daire2.YazdirCevre();
+        Daire daire3=new Daire(7);
+        //.yaricap=4;
+        daire3.YazdirAlan();
+        daire3.YazdirCevre();
+        Daire daire4=new Daire(8);
+        //
+        daire4.YazdirAlan();
+        daire4.YazdirCevre();
+        
     }
 }
