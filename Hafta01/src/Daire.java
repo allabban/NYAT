3991d7a74226a2f0ba4b06888e4bb60c3fc6b065
allabban <abdallahallabban12@gmail.com>
2025-02-20 15:11:21 +0300

    2

---
+++ Hafta01/src/Daire.java
@@ -0,0 +1,9 @@
+public class Daire {
+    double yaricap;
+    double AlanHisapla(){
+        return Math.PI * yaricap * yaricap;
+    }
+    Double CevreHisapla(){
+        return 2 * Math.PI * yaricap;
+    }
+}
